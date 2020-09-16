9463f94814b9182141fdee5d5588e68b5c666f22
melomanos <philippov_mihail@mail.ru>
2020-09-16 17:37:36 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticketCost = 13825;
+        int bonusMiles = ticketCost / 20;
+
+        System.out.println(bonusMiles);
+    }
+}
